ee50119fc8951aa8cbab11cabb3527ff587bdd54
Caleb Phillips <csphillips5@gmail.com>
2021-10-16 19:21:00 -0400

    8.3 implement query allAuthors, returns author info, include bookCount

--- index.js
+++ index.js
@@ -88,6 +88,13 @@ let books = [
 ]
 
 const typeDefs = gql`
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+    bookCount: Int!
+  }
+
   type Book {
     title: String!
     published: Int!
@@ -100,6 +107,7 @@ const typeDefs = gql`
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -107,7 +115,13 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => authors
+
+  },
+  Author: {
+    bookCount: (root) =>
+      books.filter((b) => b.author === root.name).length
   }
 }
 
